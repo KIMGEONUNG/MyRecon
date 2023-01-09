add0b4a897850f269ed48629c46ceabc36f9a38e
comar <saywooong@gmail.com>
2023-01-09 22:16:11 +0900

    feat: trivial

--- models/Low2HighNet.py
+++ models/Low2HighNet.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn.functional as F
 from torchvision.transforms import ToPILImage
 from .UNet import UNetModel
+from pycomar.images import rgb2lab, lab2rgb
 
 
 class Low2HighNet(pl.LightningModule):
@@ -18,12 +19,16 @@ class Low2HighNet(pl.LightningModule):
         x_hat = self.model(x_low)
 
         # LOSS
-        loss = nn.functional.mse_loss(x_hat * mask, x_high * mask)
+        loss = self.fn_loss(x_low, x_hat, x_high, mask)
 
         # LOGGING
         self.log("train/mse", loss)
         if batch_idx % 40 == 0:
-            self.log_img(x, x_low, x_high, x_hat, key="train/result")
+            fusion_low = self.change_luma(x, x_low)
+            fusion_high = self.change_luma(x, x_high)
+            fusion_hat = self.change_luma(x, x_hat)
+            self.log_img(x, x_low, x_high, x_hat, key="train/result_gray")
+            self.log_img(x, fusion_low, fusion_high, fusion_hat, key="train/result_fusion")
 
         return loss
 
@@ -32,12 +37,39 @@ class Low2HighNet(pl.LightningModule):
         x_hat = self.model(x_low)
 
         # LOSS
-        loss = nn.functional.mse_loss(x_hat * mask, x_high * mask)
+        loss = self.fn_loss(x_low, x_hat, x_high, mask)
 
         # LOGGING
         self.log("valid/mse", loss, sync_dist=True)
         if batch_idx % 4 == 0:
-            self.log_img(x, x_low, x_high, x_hat, key="valid/result")
+            fusion_low = self.change_luma(x, x_low)
+            fusion_high = self.change_luma(x, x_high)
+            fusion_hat = self.change_luma(x, x_hat)
+            self.log_img(x, x_low, x_high, x_hat, key="valid/result_gray")
+            self.log_img(x, fusion_low, fusion_high, fusion_hat, key="valid/result_fusion")
+
+        return loss
+
+    def change_luma(self, rgb, luma):
+        # RERANGE
+        rgb = (rgb + 1) / 2
+        luma = (luma + 1) * 50
+
+        # EXTRACT CHROMA FROM RGB
+        ab = rgb2lab(rgb)[:, 1:, :, :]
+
+        # MERGE
+        lab = torch.cat([luma, ab], dim=-3)
+        results = lab2rgb(lab)
+        results = results * 2 - 1
+
+        return results
+
+    def fn_loss(self, x_low, x_hat, x_high, mask):
+        mask_i = mask == False
+        loss_high = nn.functional.mse_loss(x_hat * mask, x_high * mask)
+        # loss_stay = nn.functional.mse_loss(x_hat * mask_i, x_low * mask_i)
+        loss = loss_high # + loss_stay
 
         return loss
 
